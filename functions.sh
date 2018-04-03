1fc12ae63145104bb3e42106780995274b3338c9
Steve Leak <sleak@lbl.gov>
2018-04-02 17:52:05 -0700

    Add exitcode to nodehistory report

--- functions.sh
+++ functions.sh
@@ -139,7 +139,8 @@ user ()
 # show what jobs have run on a given node or list of nodes, during the last day
 nodehistory () 
 { 
-  sacct --node=$1 --format=start,end,job,jobname,user,account,ncpus,nodelist -X  
+  #sacct --node=$1 --format=start,end,job,jobname,user,account,ncpus,nodelist,exitcode -X  
+  SLURM_TIME_FORMAT='%s' sacct --node=$1 --format=start,end,job,jobname,user,account,ncpus,nodelist,exitcode -X | awk 'NR<=2 {print "           " $0 ;} NR>2 { key=$1 ; $1=strftime("%Y-%m-%d-%H:%M:%S", $1); $2=strftime("%Y-%m-%d-%H:%M:%S", $2); print key "    " $0 | "column -t | sort -sn -k1"}'
 }
 
 jobsummary () 
